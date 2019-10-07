ba2f1db2c25e5f1bd6d4e282a1e76741f0dbf7c0
killerrabbit3 <stonemaa@nmsu.edu>
2019-10-07 14:27:01 -0600

    APO exposure time calc package

---
+++ APOexptime/APOexptime.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  7 14:25:10 2019
+
+@authors: Alexander
+"""
+
---
+++ APOexptime/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  7 13:48:08 2019
+
+@author: Alexander
+"""
+
