81f2c19c0c3f5f37b70d462d412f259108009590
rochoa99 <55639532+rochoa99@users.noreply.github.com>
2019-10-07 14:31:49 -0600

    Update
    Testing adding instrument class

--- APOexptime/APOexptime.py
+++ APOexptime/APOexptime.py
@@ -5,3 +5,11 @@ Created on Mon Oct  7 14:25:10 2019
 @authors: Alexander
 """
 
+class Instrument:
+    def __init__(self, Instr_name, Instr_transmission, Instr_efficiency,
+                 readout_noise, filter_wavelengths):
+        self.filename = 
+        self.transmission = 
+        self.efficiency = 
+        self.readout_noise = 
+        self.filter_wavelengths =
